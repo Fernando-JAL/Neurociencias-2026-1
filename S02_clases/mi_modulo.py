d800273cc7835d0259aba625787fbfe71f445d57
MJoJo84 <mjojo@comunidad.unam.mx>
2025-11-26 23:11:44 -0600

    Parcial3

---
+++ S02_clases/mi_modulo.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+import networkx as nx
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+def lista_dfs(ensayo_dict):
+    ensayos_dfs = []
+    for sujeto in ensayo3_dict.keys():
+        ensayos_dfs.append(ensayo3_dict[sujeto])
+    return ensayos_dfs
+
+
+
